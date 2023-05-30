b9918898787a8d515f3c1f55c31629fbb35d165f
AFZL210 <tricktomade@gmail.com>
2023-05-30 17:10:47 +0530

    init

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+double EPS = 1e-9;
+int INF = 1000000005;
+long long INFF = 1000000000000000005LL;
+double PI = acos(-1);
+
+
+int main() {
+
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+
+    cout<<"BEST ML MODEL\n";
+    
+    return 0;
+}
