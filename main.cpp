a8eac3ea83debe6377a5cdf0351fe0c5b07b0881
Mohd Afzal Khan <tricktomade@gmail.com>
2023-05-30 17:22:17 +0530

    init 2

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 double EPS = 1e-9;
@@ -6,13 +6,41 @@ int INF = 1000000005;
 long long INFF = 1000000000000000005LL;
 double PI = acos(-1);
 
+double w = 0.00;
+
+class BestMLModel {
+    vector<vector<int>> trainData;
+    int trainingSize;
+
+public:
+    BestMLModel() {
+        cout << "Enter the number of values in the training dataset: \n";
+        cin >> trainingSize;
+    }
+
+private:
+    void inputData() {
+    }
+
+    void trainModel() {
+    }
+
+    int cost() {
+
+    }
+
+    int guessValue() {
+
+    }
+};
 
 int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
+    cout << "BEST ML MODEL\n";
 
-    cout<<"BEST ML MODEL\n";
-    
+    BestMLModel newModel;
+
     return 0;
 }
